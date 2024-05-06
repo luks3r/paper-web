8c46aedb240ef5f448b840e51bfd009142ce1b96
Sergei Lukaushkin <sergeilukaushkin@gmail.com>
2024-05-06 22:17:35 +0300

    fix: proper env

--- src/services/constants.ts
+++ src/services/constants.ts
@@ -1,6 +1,4 @@
-import { env } from "process";
-
-const BASE_URL = env.PAPER_API_URL || "http://localhost:3030";
+const BASE_URL = process.env.PAPER_API_URL;
 export const BASE_API = `${BASE_URL}/api/v1`;
 export const api = {
   devices: `${BASE_API}/devices`,
